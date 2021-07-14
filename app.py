668125fa22ddecb5ebc14476164192ad211642dd
siddhantpallod <68698846+siddhantpallod@users.noreply.github.com>
2021-07-14 08:48:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+import random
+
+app = Flask(__name__)
+
+templates = [
+    {
+        'inputs': 5,
+        'category': 'Sports',
+        'word': 'Chess'
+    },
+    {
+        'inputd': 6,
+        'category': 'European Country Name',
+        'word': 'France'
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get-template')
+def getTemplate():
+    return jsonify({
+        'status': 'success',
+        'word': random.choice(templates)
+    })
+
+if __name__ == '__main__':
+    app.run()
